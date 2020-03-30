4f9960e2717db841c7024d8ff1f51069ba01310f
Chris Balow <cbalow@edmentum.com>
2020-03-30 19:57:01 -0400

    start of module 4

---
+++ src/Data/CampMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(camp => camp.Venue, opt => opt.MapFrom(src => src.Location.VenueName));
+            this.CreateMap<Talk, TalkModel>();
+            this.CreateMap<Speaker, SpeakerModel>();
+        }
+    }
+}
