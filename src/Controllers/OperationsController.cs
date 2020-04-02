876ce05c8446ed6dfe520a54a1c623534bb6fd04
Chris Balow <cbalow@edmentum.com>
2020-04-02 09:17:48 -0400

    start of module 7

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _Configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public ActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_Configuration;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
